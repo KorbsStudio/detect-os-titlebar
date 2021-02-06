286d71a584e97b452fdc349d976ee10de9c42a8c
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-02-05 19:02:10 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path');
 
+var osvar = process.platform;
+
+if (osvar == 'darwin') {
+    console.log("you are on a mac os");
+}else if(osvar == 'win32'){
+    console.log("you are on a windows os")
+}else{
+    console.log("unknown os")
+}
+
+
 if (path.sep === "\\") {
     app.whenReady().then(() => {createWindowWin()}) /* If the OS is Windows, it will run "createWindowWin" */
 } else {
@@ -17,23 +28,32 @@ function createWindowWin () {
     }
   })
   mainWindow.loadFile('index.html')
+}
 
+function createWindowMac () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.loadFile('mac.html')
 }
 
 function createWindowLinux () {
-    const mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      frame: true,
-      autoHideMenuBar: true,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
-  
-    mainWindow.loadFile('index.html')
-  
-  }
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.loadFile('index.html')
+}
 
 
 
